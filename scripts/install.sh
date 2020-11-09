93afba7c392c8b9af2e8724847a97d7f1c8d78c8
Diego Cartagena <dlcartagena@uc.cl>
2020-11-08 23:38:14 -0300

    scripts modified

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@
 pwd=$( aws ecr get-login-password )
 docker container stop $(docker container ls -aq)
 docker login -u AWS -p $pwd 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
-docker pull 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8/ChatApp-backend
+docker pull 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo "${pwd}"
 docker-compose -f /home/ubuntu/app/ChatApp/docker-compose.yml up -d
