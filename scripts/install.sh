9853bc3e11bdc55b65114b58bda79e0108d12043
Diego Cartagena <dlcartagena@uc.cl>
2020-11-08 23:52:36 -0300

    ahora si

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pwd=$( aws ecr get-login-password )
 docker container stop $(docker container ls -aq)
-docker login -u AWS -p $pwd 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
+docker login -u AWS --password-stdin $pwd 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
 docker pull 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $(pwd)
-docker-compose -f /home/travis/build/dlcartagena/ChatApp/docker-compose.yml up -d
+docker-compose -f /home/travis/build/dlcartagena/ChatApp/docker-compose.production.yml up -d
