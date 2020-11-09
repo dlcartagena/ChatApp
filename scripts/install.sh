5fea21b6c4536165cbaea28885577e879087be7e
Diego Cartagena <dlcartagena@uc.cl>
2020-11-08 23:33:52 -0300

    install script modified

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pwd=$( aws ecr get-login-password )
 docker container stop $(docker container ls -aq)
-docker login -u AWS -p $pwd https://xxxxxxxxxxxxx.dkr.ecr.sa-east-1.amazonaws.com
-docker pull xxxxxxxxxx.dkr.ecr.sa-east-1.amazonaws.com/ChatApp-backend
-
+docker login -u AWS -p $pwd 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
+docker pull 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8/ChatApp-backend
