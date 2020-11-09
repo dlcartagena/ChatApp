860ed2769a5f3f708ae293cf693dab806151a223
Diego Cartagena <dlcartagena@uc.cl>
2020-11-09 17:32:06 -0300

    conf changes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 docker --version
 pip install --user aws-cli
 export PATH=$PATH:$HOME/.local/bin
-eval $(aws ecr get-login --region sa-east-1 --no-include-email)
+eval $(aws ecr get-login --region us-east-1 --no-include-email)
 docker build -t $AWS_ECR_API:latest
 docker tag $AWS_ECR_API:latest
 docker push $AWS_ECR_API:latest
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,5 @@
 pwd=$( aws ecr get-login-password )
 
 docker container stop $(docker container ls -aq)
-docker login -u AWS --password-stdin $pwd 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
-docker pull 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
+docker login -u AWS --password-stdin $pwd $AWS_ACCESS_KEY_ID.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
+docker pull $AWS_ACCESS_KEY_ID.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
