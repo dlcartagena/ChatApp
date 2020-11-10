276c3c19dd7d2650d31dcb1831cff7b08d7b074d
Diego Cartagena <dlcartagena@uc.cl>
2020-11-09 23:38:23 -0300

    scripts added

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker --version
-eval $(aws ecr get-login --region us-east-1 --no-include-email)
-docker build -t $AWS_ECR_API:latest
-docker tag $AWS_ECR_API:latest
-docker push $AWS_ECR_API:latest
-docker images
