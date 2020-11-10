a9ae9f7ec72643fe7b2da0255472bd4f0cfb322c
Diego Cartagena <dlcartagena@uc.cl>
2020-11-10 00:02:41 -0300

    tests script modified

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+docker-compose -f /home/travis/build/dlcartagena/ChatApp/docker-compose.yml build
+docker-compose up -d
 docker-compose -f /home/travis/build/dlcartagena/ChatApp/docker-compose.yml exec app npm test
+docker-compose down
