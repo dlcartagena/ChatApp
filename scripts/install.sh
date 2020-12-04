656e7a4b9eca22392a596f0e64accc768dd76fec
IRINA SALAZAR <iasalazar@uc.cl>
2020-12-03 22:23:49 -0300

    docker in ec2

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo docker container stop $(docker container ls -aq)
+docker container stop $(docker container ls -aq)
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_USER_ID.dkr.ecr.us-east-1.amazonaws.com
-sudo docker pull $AWS_ACCESS_KEY_ID.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
+docker pull $AWS_ACCESS_KEY_ID.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker-compose -f /home/ubuntu/app/docker-compose.travis.yml down
-sudo docker stop $(docker ps -a -q)
+docker stop $(docker ps -a -q)
