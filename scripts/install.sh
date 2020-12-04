bcaacdce1195337acea08e3d348893d43d8ecf8e
IRINA SALAZAR <iasalazar@uc.cl>
2020-12-04 10:57:22 -0300

    bai stops hola num id

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${AWS_USER_ID}.dkr.ecr.us-east-1.amazonaws.com
-docker pull ${AWS_ACCESS_KEY_ID}.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 922803931776.dkr.ecr.us-east-1.amazonaws.com
+docker pull 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
