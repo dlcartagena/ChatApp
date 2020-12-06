7bc0351d6f632a165ee62906610571239fa5415e
IRINA SALAZAR <iasalazar@uc.cl>
2020-12-06 02:34:50 -0300

    what

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+docker container stop $(docker container ls -aq)
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 922803931776.dkr.ecr.us-east-1.amazonaws.com
 docker pull 922803931776.dkr.ecr.us-east-1.amazonaws.com/iic2173-proyecto-semestral-grupo-8
